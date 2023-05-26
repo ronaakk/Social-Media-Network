be293888763304a1ef78e46c38ee5ad81aaa3cec
Ronak Patel <ronaakk6@gmail.com>
2023-05-26 13:20:14 -0400

    commit

---
+++ network/static/network/js/index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const post = document.querySelector('#post');
+
+
+    const posts = document.querySelector(".posts");
+
+
+
+
+
+
+
+
+
+
+
+})
