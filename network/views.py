1d2e71c47857aba27ca2d0a0f5042a17593a25cf
Ronak Patel <ronaakk6@gmail.com>
2023-08-11 14:19:04 -0400

    redirect changes

--- network/views.py
+++ network/views.py
@@ -27,7 +27,7 @@ def index(request):
     except:
         user_profile = None
 
-    return render(request, "network/about.html", {
+    return render(request, "network/index.html", {
         "user_profile" : user_profile,
     })
 
